62d1e0b0df2c35c6e5d080fec8fd44b8509e8c61
Clayton Smith <claybin@claytons-MacBook-Pro.local>
2016-11-23 07:11:46 -0700

    submits/ page does nto refresh

--- starter-code/app/js/jquery.app.js
+++ starter-code/app/js/jquery.app.js
@@ -2,14 +2,27 @@ var array=[];
 
 var catObject = $.get('https://ga-cat-rescue.herokuapp.com/api/cats')
 .done(function(data){
-console.log(data);
+//console.log(data);
 array = jQuery.parseJSON(catObject.responseText);
 	for (var i = 0; i < array.length; i++) {
 	
-	$('#cats').append('<li>'+array[i].name +' ' + array[i].note+'</li>');
-
+	$('#cats').append('<li>'+array[i].name +': ' + array[i].note+'</li>');
 	}
-
-console.log(array.val);
 }); 
 
+
+$(document).ready(function(){
+	$('form').submit(function(e) {
+		var $cName = $("#cat-name").val();
+		var $cNote = $("#cat-note").val();
+		$.ajax({
+			type: "POST",
+			url: "https://ga-cat-rescue.herokuapp.com/api/cats",
+			data: JSON.stringify({ 'name': $cName, 'note': $cNote }),
+			dataType: "json"
+		}).done(function(cat) {
+			console.log(cat);
+		});
+		e.preventDefault();
+	});
+});
